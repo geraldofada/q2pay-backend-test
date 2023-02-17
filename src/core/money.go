08f2cbed05ce2a99cbd5f6ef0c67fe64d81fe7fc
Geraldo Fada <geraldofada.neto@gmail.com>
2023-02-17 19:04:21 -0300

    feat: format method for Money

--- src/core/money.go
+++ src/core/money.go
@@ -1,5 +1,9 @@
 package core
 
+import (
+	"fmt"
+)
+
 const (
 	BRL Currency = "BRL"
 	USD Currency = "USD"
@@ -11,3 +15,19 @@ type Money struct {
 	Amount   int64
 	Currency Currency
 }
+
+// TODO: talvez lidar com valores negativos? nunca deveria acontecer, mas sabe-se lá
+func (m Money) Format() string {
+	reals := m.Amount / 100
+	cents := m.Amount % 100
+
+	if cents < 10 {
+		return fmt.Sprintf("%s %d,0%d", m.Currency, reals, cents)
+	} else {
+		return fmt.Sprintf("%s %d,%d", m.Currency, reals, cents)
+	}
+}
+
+// TODO
+// func ParseStringToMoney(money string) Money {
+// }
