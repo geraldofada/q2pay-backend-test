456d81daf839c1c832e884cbd4afe3e60032db66
Geraldo Fada <geraldofada.neto@gmail.com>
2023-02-17 11:49:11 -0300

    refactor: renamed use cases to be more semantic

--- src/port/use_case.go
+++ src/port/use_case.go
@@ -3,8 +3,8 @@ package port
 import "github.com/geraldofada/q2pay-backend-test/src/core"
 
 type PayeeUseCase interface {
-	Login(email string, password string) (core.Payee, error)
-	Signup(name string, email string, doc string, password string) (core.Payee, error)
+	PayeeLogin(email string, password string) (core.Payee, core.Token, error)
+	PayeeSignup(name string, email string, doc string, password string) (core.Payee, error)
 }
 
 type AuthUseCase interface {
